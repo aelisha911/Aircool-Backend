f28722c5f536639dd4c829fbd623c478a3e8e2b0
aelisha911 <aelishapatel426@gmail.com>
2026-05-27 16:33:38 +0530

    minor

--- app.js
+++ app.js
@@ -14,13 +14,18 @@ const app = express();
 // ✅ VERY IMPORTANT MIDDLEWARES
 const frontendOrigins = process.env.FRONTEND_URLS
   ? process.env.FRONTEND_URLS.split(",").map((origin) => origin.trim())
-  : ["http://localhost:8080/", "https://aircool-frontend-d13o.onrender.com"];
+  : [
+      "http://localhost:8080",
+      "https://aircool-frontend-d13o.onrender.com",
+    ];
 
-app.use(cors({
-  origin: frontendOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: frontendOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 // Database Connection
 connectDB().catch((err) => {
   console.log("DB Connection Error:", err);
@@ -42,9 +47,9 @@ app.get("/", (req, res) => {
   res.send("Aircool dynamics Backend Running 🚀");
 });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () =>
-//   console.log(`Server running on port ${PORT}`)
-// );
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
 
 export default app;
